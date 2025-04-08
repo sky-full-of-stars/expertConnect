090db9ea0380988213bbfade22f57d23474f3339
Akshay Arga Suryanarayan <akshay@Akshays-MacBook-Air.local>
2025-04-08 20:12:45 +0530

    pushing repo

---
+++ expertConnect/src/main/java/com/uci/expertConnect/ExpertConnectApplication.java
@@ -0,0 +1,13 @@
+package com.uci.expertConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpertConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpertConnectApplication.class, args);
+	}
+
+}
