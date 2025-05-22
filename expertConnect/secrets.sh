4f1696d6062d2f79c3fc7540bd0a6b7b9539f508
akshay <akshay.suryanarayan@gmail.com>
2025-05-21 20:59:51 -0700

    Initial commit without secrets

---
+++ expertConnect/secrets.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# OpenAI API Key
+export OPENAI_API_KEY="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"
+
+# AWS Credentials
+export AWS_ACCESS_KEY_ID="AKIAWWK7Y4PNSTUP3D4O"
+export AWS_SECRET_ACCESS_KEY="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"
+export AWS_REGION="us-east-2"
+export AWS_S3_BUCKET="expertconnect-profile-photos"
+
+echo "Environment variables have been set successfully!" 
